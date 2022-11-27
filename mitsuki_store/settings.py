8541a3e2f7e12d1e97244ba64e55821c7a9c1b4e
mitsuki <k16nt64@gmail.com>
2022-11-27 10:17:57 +0900

    DEBUG = False

--- mitsuki_store/settings.py
+++ mitsuki_store/settings.py
@@ -28,7 +28,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
